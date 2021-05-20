562412bc51c912fe9e59b22d03a60dfc9ad6547d
user <elmashavitran@gmail.com>
2021-05-20 14:32:46 +0300

    1st

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const apiCallFromNode = require('./nodeCalls');
 
 
 const firebase = require("firebase");
-require("firebase/firestore");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+const db = admin.firestore();
 // with ES Modules (if using client-side JS, like React)
 
 
