186e73e060f750bde060dbf082fc24ffc8277a0f
user <elmashavitran@gmail.com>
2021-05-20 11:58:20 +0300

    1st

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ app.get('/stk_callback',_urlencoded,function(req,res,next){
 
     console.log('.......... STK Callback ..................');
     if(res.status(200)){
-        res.json((req.body.Body.stkCallback.CallbackMetadata.Item[1].Value))
+        res.json((req.body.Body.stkCallback.CallbackMetadata.Item))
         
         console.log(req.body.Body.stkCallback.CallbackMetadata.Item[1].Value)
 
