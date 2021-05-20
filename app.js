d40f7a13b6f4b1c44a3a689f90762713911a2040
user <elmashavitran@gmail.com>
2021-05-20 12:17:35 +0300

    1st

--- app.js
+++ app.js
@@ -150,8 +150,9 @@ app.post('/stk_callback',_urlencoded,function(req,res,next){
     if(res.status(200)){
         res.json((req.body.Body.stkCallback.CallbackMetadata.Item[0].Value))
         
+        console.log(req.body.Body.stkCallback.CallbackMetadata)
         console.log(req.body.Body.stkCallback.CallbackMetadata.Item[0].Value)
-        console.log(req.body.Body.stkCallback.CallbackMetadata.Item[2].Value)
+        console.log(req.body.Body.stkCallback.CallbackMetadata.Item[1].Value)
         console.log(req.body.Body.stkCallback.CallbackMetadata.Item[2].Value)
 
     const data ={
