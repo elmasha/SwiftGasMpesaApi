40a663ca16b7ea7f4e1c9d73ebc2d9774d0273b6
user <elmashavitran@gmail.com>
2021-05-28 12:58:12 +0300

    1st

--- firebase/setData.js
+++ firebase/setData.js
@@ -6,11 +6,11 @@ var transNo = '';
 var userName = '';
 module.exports ={
     stk_callback: function(req,callback){
-        
-        amount = req.body.Body.stkCallback.CallbackMetadata.Item[0].Value;
-        transID = req.body.Body.stkCallback.CallbackMetadata.Item[1].Value;
-        transNo = req.body.Body.stkCallback.CallbackMetadata.Item[4].Value;
-        transdate = req.body.Body.stkCallback.CallbackMetadata.Item[3].Value;
+
+        amount = req.body.stkCallback.CallbackMetadata.Item[0].Value;
+        transID = req.body.stkCallback.CallbackMetadata.Item[1].Value;
+        transNo = req.body.stkCallback.CallbackMetadata.Item[4].Value;
+        transdate = req.body.stkCallback.CallbackMetadata.Item[3].Value;
        
         firebase.collection("Payments_backup").add({
             TransID : transID ,
