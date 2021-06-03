1d60c624a738b72975e82b9afd0c7ef5b8a57709
elmasha <elmashavitran@gmail.com>
2021-06-03 03:19:02 +0300

    1

--- app.js
+++ app.js
@@ -299,7 +299,7 @@ app.post('/stk_callback',_urlencoded,middleware,function(req,res,next){
             Vendor_Name: _VendorName,
             Shop_Name : _ShopName,
             Shop_No : _ShopNo ,
-            timestamp: _Timestamp,
+            timestamp: "",
         }).then((ref) => {
 
     
