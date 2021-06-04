71493b02a223444903c0c4b16b2617ac9cefb52a
user <elmashavitran@gmail.com>
2021-06-04 12:49:37 +0300

    1

--- app.js
+++ app.js
@@ -773,7 +773,7 @@ app.post('/stk_callbackActivate',_urlencoded,middleware3,function(req,res,next){
        console.log("Transaction",_Number)
        console.log("TransactionTime",transdate)
 
-   
+       var batch = db.batch();
        var boost = db.collection("Gas_Vendor").doc(id);
 
        batch.update(boost,{"Activation_fee":150});
