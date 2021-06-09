07ac5e0edbcb4eade1f02297ef7c6cad43b39ab7
user <elmashavitran@gmail.com>
2021-06-09 17:12:17 +0300

    1

--- app.js
+++ app.js
@@ -316,7 +316,27 @@ app.post('/stk_callback',_urlencoded,middleware,function(req,res,next){
         
                     console.log("Order added doc with ID: ", id);
                     console.log("Order is", _Name ," ",_item_desc);
-        
+                     Userid = '';
+                     _checkoutID = '';
+                     _Name = '';
+                     _Category = '';
+                     _CustomerNo = '';
+                     _CustomerName = '';
+                     _ItemDesc = '';
+                     _ItemImage = '';
+                     _PaymentMethod = '';
+                     _Price = '';
+                     _Quantity = '';
+                     _Rated = '';
+                     _ShopName = '';
+                     _ShopNo = '';
+                     _UserImage = '';
+                     _VendorId = '';
+                     _VendorName = '';
+                     _Lat = '';
+                     _Lng = '';
+                     _OrderStatus =  '';
+                     _ExCylinder = '';
                 });
             }
 
@@ -343,6 +363,7 @@ app.post('/stk_callback',_urlencoded,middleware,function(req,res,next){
                 timestamp : transdate,
                 User_id : Userid,
             }).then((ref) => {
+                console.log("Added doc with ID: ", transID);
                 console.log("Added doc with ID: ", transID);
             });
     
